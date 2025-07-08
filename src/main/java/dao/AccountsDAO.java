d21cd779aaa63b95f08f01fc97ce5c0f6eb79f54
sn <sn@SNPC2352>
2025-07-08 13:31:08 +0900

    [古市]daoとmodel
    Merge branch 'master' of https://github.com/kiyotaka0819/Buzz.git

---
+++ src/main/java/dao/AccountsDAO.java
@@ -0,0 +1,13 @@
+package dao;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class AccountDAO
+ */
+@WebServlet("/AccountsDAO")
+public class AccountsDAO extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
