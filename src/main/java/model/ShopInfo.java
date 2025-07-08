8ed90b96b8dab1ef25765b0575abb154f76007a6
sn <sn@SNPC2357>
2025-07-08 16:47:45 +0900

    DAO制作中

---
+++ src/main/java/model/ShopInfo.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.io.Serializable;
+
+public class ShopInfo implements Serializable {
+
+	private String shopName;
+	private String shopAddress;
+	private String shopURL;
+	private String shopTEL;
+	
+	public ShopInfo(String shopName,String shopAddress, String shopURL, String shopTEL) {
+		this.shopName = shopName;
+		this.shopAddress = shopAddress;
+		this.shopURL = shopURL;
+		this.shopTEL = shopTEL;
+	}
+	public String getShopName() {
+		return shopName;
+	}
+	public String getShopAddress() {
+		return shopAddress;
+	}
+	public String getShopURL() {
+		return shopURL;
+	}
+	public String getShopTEL() {
+		return shopTEL;
+	}
+}
+
