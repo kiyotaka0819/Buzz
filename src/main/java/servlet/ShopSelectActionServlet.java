3d07d30adc6a8a5a3106330f550f25526ba7a775
sn <sn@SNPC2357>
2025-07-17 13:42:32 +0900

    【天本】PostとPostEditを分岐して、店舗選択から帰れるように

--- src/main/java/servlet/ShopSelectActionServlet.java
+++ src/main/java/servlet/ShopSelectActionServlet.java
@@ -12,32 +12,35 @@ import jakarta.servlet.http.HttpSession;
 @WebServlet("/ShopSelectActionServlet")
 public class ShopSelectActionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String shopName = request.getParameter("shopName");
-        HttpSession session = request.getSession();
+		HttpSession session = request.getSession();
+
 		
+		session.setAttribute("selectedShopForPost", shopName);
+
 		// 選択した店舗名をセッションに保存
-        session.setAttribute("selectedShopForPost", shopName);
-
-        // 編集か投稿かを示すセッション情報を取得
-        String redirectBackTo = (String) session.getAttribute("redirectBackTo");
-        
-        // セッションから取得して文字列に変換
-        String editingPostId = String.valueOf(session.getAttribute("editingPostId")); 
-        
-        // 使い終わったセッションは削除する。
-        session.removeAttribute("editingPostId");
-        // セッション情報を削除(redirectBackToだけにする)
-        session.removeAttribute("redirectBackTo");
-
-        if ("PostEditServlet".equals(redirectBackTo)) {
-            // 編集画面から来た場合、編集画面にリダイレクト
-            response.sendRedirect(request.getContextPath() + "/PostEditServlet?postId=" + editingPostId);
-        } else {
-            // 通常の投稿画面から来た場合、投稿画面にリダイレクト
-            response.sendRedirect(request.getContextPath() + "/PostServlet");
-        }
-    }
+		String redirectBackTo = (String) session.getAttribute("redirectBackTo");
+		// セッションから取得して文字列に変換
+		Object editingPostIdObj = session.getAttribute("editingPostId");
+	    String editingPostId = null;
+	    if (editingPostIdObj != null) {
+	        editingPostId = String.valueOf(editingPostIdObj);
+	    }
+		if ("PostEditServlet".equals(redirectBackTo)) {
+			// 編集画面から来た場合、編集画面にリダイレクト
+			// リダイレクトする前にセッションをクリア
+			session.removeAttribute("editingPostId");
+			session.removeAttribute("redirectBackTo");
+			response.sendRedirect(request.getContextPath() + "/PostEditServlet?postId=" + editingPostId);
+		} else {
+			// 通常の投稿画面から来た場合、投稿画面にリダイレクト
+			session.removeAttribute("editingPostId"); // nullが入ってる可能性があるので念のため
+			session.removeAttribute("redirectBackTo"); // nullが入ってる可能性があるので念のため
+			// PostServletにリダイレクトする時にパラメータを付ける
+			response.sendRedirect(request.getContextPath() + "/PostServlet?fromShopSelect=true");
+		}
+	}
 }
